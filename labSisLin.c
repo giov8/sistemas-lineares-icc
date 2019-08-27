067340138f11bf7e0371cdcf4d6382e4b53d8a1d
ggm18 <ggm18@inf.ufpr.br>
2019-08-27 14:23:54 -0300

    Primeiro Commit

---
+++ labSisLin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "utils.h"
+#include "SistemasLineares.h"
+
+int main ()
+{
+    // inicializa gerador de nr aleatoreos
+    srand(20192);
+    
+  SistLinear_t *SL = alocaSistLinear(5);
+  inicializaSistLinear (SL, 0, 12);
+  prnSistLinear (SL);
+
+  real_t *x = malloc(SL->n * sizeof(double));
+
+  eliminacaoGauss(SL, x, 1);
+}
+
